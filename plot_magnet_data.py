cd44ae3464611ff7374ee18d765842a8f168fc8c
Dragos Ileana <D.Ileana@student.tudelft.nl>
2025-11-01 01:39:03 +0100

    Change code for magnet data plot

--- plot_magnet_data.py
+++ plot_magnet_data.py
@@ -1,19 +1,20 @@
-import numpy as np
-import pandas as pd
+from pandas import read_csv
 import matplotlib.pyplot as plt
 
-path = r"C:\Users\niant\Documents\Qist\MDP\Magnet-Positioner-Project\magnet_data\{number}.csv"
-average = []
-for i in range(50):
-    print(i)
-    if i > 17: #indexing misaligned from 18 onwards
-        i += 1
-    df = pd.read_csv(path.format(number=i), sep = ';', skiprows=7)
-    df1 = df['By[mT]']
-    average.append(abs(df1.mean())) #save the absolute magnetic field value
-print(average)
-distance = np.arange(50)
-plt.plot(distance, average) #plot the stuff
-plt.xlabel('Distance from chip center [cm]')
-plt.ylabel('Mean field [mT]')
+
+# Read measurements from CSV file
+df = read_csv(r'./magnet_data/magnet_x-axis_measurement_data.csv')
+
+# Select columns
+df = df[['Magnet-sensor distance [cm]', r'By[mT]']]
+# Group by distance
+df = df.groupby(['Magnet-sensor distance [cm]'])
+# Compute mean B field at each distance. Take the absolute value.
+means = df.mean().abs()
+
+# Plot
+plt.title('Magnet field measurements with Hall sensor')
+plt.plot(means)
+plt.xlabel('Magnet-sensor distance [cm]')
+plt.ylabel('Mean magnetic field [mT]')
 plt.show()
